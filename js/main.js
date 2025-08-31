36190f2da9d8d257274f9b9526a59181fa70f9fe
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-08-31 11:56:02 +0530

    load csv data

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-fetch("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json")
-.then(response=>{
-    if(!response.ok){
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-})
-.then(data=>{
-    console.log(data);
-})
-.catch(error=>{
-    console.error('Error fetching weather data:', error);
-});
+
+let cities = [];
+
+function loadCitiesData() {
+    return new Promise((resolve, reject) => {
+        Papa.parse('../city_coordinates.csv', {
+            download: true,
+            header: true,
+            dynamicTyping: true,
+            complete: function(results) {
+                console.log("CSV data loaded successfully:", results.data);
+                cities = results.data;
+                resolve();
+            },
+            error: function(err) {
+                console.error("Error parsing CSV:", err);
+                reject(err);
+            }
+        });
+    });
+}
+
+window.onload = function(){
+    loadCitiesData();
+}
