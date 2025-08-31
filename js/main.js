917f60876b84c6cf29110553fad422046df660d7
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-08-31 14:50:17 +0530

    get input from text box and call Weather API

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ function loadCitiesData() {
 window.onload = function(){
     loadCitiesData().then(() => {
         populateCityList();
+        setupSearch();
     });
 }
 
@@ -35,4 +36,29 @@ function populateCityList(){
         option.value = `${city.city}, ${city.country}`;
         dataList.appendChild(option);
     })
+}
+
+function setupSearch(){
+    const searchInput = document.getElementById('citySearch');
+
+    searchInput.addEventListener('keypress',function(e){
+        if(e.key === 'Enter'){
+            e.preventDefault();
+            searchWeather();
+        }
+    })
+}
+
+function searchWeather(){
+    const searchInput = document.getElementById('citySearch').value;
+    console.log("Searching weather for:", searchInput);
+
+    const cityName = searchInput.split(",")[0].trim();
+
+    const foundCity = cities.find(city => city.city === cityName);
+    console.log("Found city lat:", foundCity.latitude);
+    console.log("Found city long:", foundCity.longitude);
+
+    const weatherUrl = `http://www.7timer.info/bin/api.pl?lon=${foundCity.longitude}&lat=${foundCity.latitude}&product=astro&output=json`;
+
 }
